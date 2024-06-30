8fdf0bf8cc0e049a8f21bae6a6514991139bd63c
pragadxxsh16 <prakadeshvicky77@gmail.com>
2024-07-01 02:39:48 +0530

    uploaded

---
+++ blog-site/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/home.css">
+    <title>HOME PAGE</title>
+</head>
+    <body>
+        
+        <h1>
+        Welcome to Blog Creation
+        </h1>
+        <p>
+        "The Place where your creativity meets Execution"
+        </p>
+        <a href="{{route('BlogSite.create')}}"><button>Let's Create</button></a>
+        
+    </body>
+</html>
